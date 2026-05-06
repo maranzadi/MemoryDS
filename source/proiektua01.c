145a7d46f928a018aecf96e1e2f4428b3b405882
Manex Arazadi <manexae@protonmail.com>
2026-05-06 13:15:17 +0200

    konponduta zegoen arazoa galtzean

--- source/proiektua01.c
+++ source/proiektua01.c
@@ -128,8 +128,9 @@ void proiektua01()
 
 				}else{
 					
-					EGOERA=ITXITA;
+					EGOERA=GALDU;
 					erakutsiKolorea(sekuentzia[input_index]);
+					galduDegu();
 				}
 			
 
@@ -145,10 +146,6 @@ void proiektua01()
 			{
 				EGOERA=ZAI;
 				consoleClear();
-				asmatuta=0;
-				zenbat=0;
-				luzera=0;
-				input_index=0;
 				lcdSwap();
 				SarreraPantailaratu();
 				erakutsiDefault();
@@ -159,21 +156,7 @@ void proiektua01()
 			
  
 		}
-		if (EGOERA==ITXITA)
-		{
-			
-			consoleClear();
-			
-			lcdSwap();
-			iprintf("\x1b[14;1HGAME OVER");	
-			iprintf("\x1b[16;1HPuntuazioa: %d", asmatuta);
-			EGOERA=GALDU;
-			asmatuta=0;
-			zenbat=0;
-			luzera=0;
-			input_index=0;
-			
-		}
+		
 	
 		// if(logEgin==LOG){
 		// 	logak();
@@ -241,6 +224,20 @@ void logak(){
 		iprintf("\x1b[%d;%dHSekuentzia: %d", non, i+2, sekuentzia[i]);
 	}
 	
+}
+
+
+void galduDegu(){
+	consoleClear();
+			
+	lcdSwap();
+	iprintf("\x1b[14;1HGAME OVER");	
+	iprintf("\x1b[16;1HPuntuazioa: %d", asmatuta);
+	EGOERA=GALDU;
+	asmatuta=0;
+	zenbat=0;
+	luzera=0;
+	input_index=0;
 }
 /***********************2025-2026*******************************/
 
--- source/zerbitzuErrutinak.c
+++ source/zerbitzuErrutinak.c
@@ -158,7 +158,8 @@ void tenpEten()
 			iprintf("\x1b[13;5HDenbora=%d", (luzera+5)-segPunt);
 			if (segPunt>luzera+5)
 			{
-				EGOERA=ITXITA;
+				EGOERA=GALDU;
+				galduDegu();
 			}
 			
 		}
